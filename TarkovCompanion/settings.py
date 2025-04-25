c4afa8a2bcc2d8d8cd1d4759c33be3d57043970d
Thom de Wit <123957198+TJWit@users.noreply.github.com>
2025-04-25 19:33:12 +0200

    created main app

--- TarkovCompanion/settings.py
+++ TarkovCompanion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w-9x*1hregp&6aes-k33)v0dgp1m!=4@+obfbsd$1ib43tuoi@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
